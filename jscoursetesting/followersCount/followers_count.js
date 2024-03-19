d016d60eea93bf8027343ca88489bb7a0853a074
Mukul <mukul71@gmail.com>
2024-03-19 00:35:54 -0400

    counters added

---
+++ jscoursetesting/followersCount/followers_count.js
@@ -0,0 +1,16 @@
+let count = 0;
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+function displayCount(){
+    document.getElementById("countDisplay").innerHTML=count;
+}
+function checkCountValue(){
+    if(count ===10){
+        alert("Your post gained 10 followers! Congratulations.");
+    } else if(count === 20){
+        alert("Your post gained 20 followers! keep it up");
+    }
+}
